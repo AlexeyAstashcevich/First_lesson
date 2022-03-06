28f69d297c510125d6294c3545546c3a7393b3c1
simpotka76 <96411523+simpotka76@users.noreply.github.com>
2022-03-06 15:50:59 +0300

    Update MyFirstProgram.java

--- SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,19 +9,19 @@ public class MyFirstProgram {
     double l = 5;
     double a = 5;
     double b = 6;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l) );
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b +" = " + area(a,b));
+      System.out.println("Площадь квадрата со стороной " + l + " = " + area(l) );
+      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b +" = " + area(a,b));
   }
     public static void hello (String somebody){
       System.out.println("Hello, " + somebody + "!" );
     }
 
     public static double area (double l){
-    return l*l;
+      return l*l;
 
   }
     public static double area (double a , double b){
-    return a * b;
+      return a * b;
 
     }
 }
