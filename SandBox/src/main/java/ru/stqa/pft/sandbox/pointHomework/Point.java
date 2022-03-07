60a2db3f7870825f1dee4bab36f56de2730d5daf
simpotka76 <96411523+simpotka76@users.noreply.github.com>
2022-03-07 19:01:40 +0300

    Домашнее задание № 2,

--- SandBox/src/main/java/ru/stqa/pft/sandbox/pointHomework/Point.java
+++ SandBox/src/main/java/ru/stqa/pft/sandbox/pointHomework/Point.java
@@ -4,6 +4,11 @@ public class Point {
   private double x;
   private double y;
 
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+
+  }
   public double getX() {
     return x;
   }
@@ -19,10 +24,10 @@ public class Point {
   public void setY(double y) {
     this.y = y;
   }
-
-  public Point(double x, double y){
-    this.x = x;
-    this.y = y;
-
+      public double getDistance(Point p){
+       return Math.sqrt(Math.pow((this.x - p.x),2) + Math.pow((this.y - p.y),2));
+  }
+  public String toString(){
+    return "(" + this.x +", " + this.y + ")";
   }
 }
