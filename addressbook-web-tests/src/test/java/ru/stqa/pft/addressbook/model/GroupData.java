0339448b078d4b3cd3c4a61edf414ef12a649924
simpotka76 <zbvnr@yandex.ru>
2022-04-21 12:54:18 +0300

    Commit Hibermate problem

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,75 +4,88 @@ import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
+
+
 @XStreamAlias("groups")
+@Entity
+@Table(name = "group_list")
 public class GroupData {
-  @Expose
-  private String name;
-  @Expose
-  private String header;
-  @Expose
-  private String footer;
- @XStreamOmitField
-  private int id;
+    @Column(name = "group_name")
+    @Expose
+    private String name;
+    @Column(name = "group_header")
+    @Expose
+    private String header;
+    @Expose
+    @Column(name = "group_footer")
+    private String footer;
+    @Column(name = "group_id")
+    @XStreamOmitField
+    @Id
+    private int id;
 
 
+    public int getId() {
+        return id;
+    }
 
-  public int getId() {
-    return id;
-  }
+    public GroupData withId(int id) {
+        this.id = id;
+        return this;
+    }
 
-  public GroupData withId(int id) {
-    this.id = id;
-    return this;
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return id == groupData.id && Objects.equals(name, groupData.name);
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id && Objects.equals(name, groupData.name);
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(name, id);
-  }
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
 
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            ", id='" + id + '\'' +
-            '}';
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public GroupData withName(String name) {
+        this.name = name;
+        return this;
+    }
 
-  public GroupData withName(String name) {
-    this.name = name;
-    return this;
-  }
+    public String getHeader() {
+        return header;
+    }
 
-  public String getHeader() {
-    return header;
-  }
+    public GroupData withHeader(String header) {
+        this.header = header;
+        return this;
+    }
 
-  public GroupData withHeader(String header) {
-    this.header = header;
-    return this;
-  }
+    public String getFooter() {
+        return footer;
+    }
 
-  public String getFooter() {
-    return footer;
-  }
-
-  public GroupData withFooter(String footer) {
-    this.footer = footer;
-    return this;
-  }
+    public GroupData withFooter(String footer) {
+        this.footer = footer;
+        return this;
+    }
 
 }
